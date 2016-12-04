052c55a89811b2edf1fe58419a03a8cb37e0eb10
Kavish Patel <kpatel6987@gmail.com>
2016-12-04 18:24:33 -0500

    Added appropriate permissions for viewing reports

--- public/js/viewCtrl.js
+++ public/js/viewCtrl.js
@@ -1,9 +1,28 @@
-var viewCtrl = angular.module('viewCtrl',['gservice']);
-viewCtrl.controller('viewCtrl', function($scope, $http, $rootScope, $location, $window, gservice){
+var viewCtrl = angular.module('viewCtrl',['gservice', 'loginService']);
+viewCtrl.controller('viewCtrl', function($scope, $http, $rootScope, $location, $window, gservice, loginService){
+
+    $scope.user = loginService.user;
+
+    $scope.checkPermission = function(user) {
+        if (user.accountType == "User") {
+            return false;
+        }
+        return true;
+    }
+
+    $scope.checkManager = function(user) {
+        if (user.accountType == "Manager") {
+            return true;
+        }
+        return false;
+    }
+
     $http.get("/sourceReportList").then(function(response) {
         $scope.sourceReports = response.data;
     });
+
     $http.get("/qualityReportList").then(function(response) {
         $scope.qualityReports = response.data;
+        console.log(response.data);
     });
 });
